2aea523d09f74deed94d3c28e914a57aa6ed37e3
Avraham Weinstein <avrahamwn@ravtech.co.il>
2021-02-15 13:32:10 +0200

    input corrections

--- src/components/SearchPage/Search-page.js
+++ src/components/SearchPage/Search-page.js
@@ -18,6 +18,11 @@ function SearchPage(props) {
     const [keyWord, setKeyWord] = useState('')
 
     function getBooks(startIndex = 0) {
+        if(!keyWord){ 
+            setBooksList([])
+            setNumOfPages(0)
+            return;
+        }
         axios.get('https://www.googleapis.com/books/v1/volumes?maxResults=15&q=' + keyWord + '&startIndex=' + startIndex)
             .then(response => {
                 setBooksList(response.data.items[0] ? response.data.items : [])
@@ -31,6 +36,8 @@ function SearchPage(props) {
 
     function onInputChange(event) {
         setKeyWord(event.target.value)
+        console.log(event);
+        console.log(keyWord);
         getBooks();
     }
 
@@ -55,8 +62,8 @@ function SearchPage(props) {
                 </WishListLink>
                 <Title className="div">Hello {props.username}!</Title>
                 <SubTitle>Here is your search screen</SubTitle>
-                <InputLabel>
-                    Please enter here the name of the book you'd like to search for
+                <InputLabel active={true}>
+                  Please enter here the name of the book you'd like to search for
             </InputLabel>
                 <Input
                     onChange={onInputChange}
